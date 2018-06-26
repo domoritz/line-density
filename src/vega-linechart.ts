7e345adc9a6c9df5d95627b5b9bae5144d2efaf1
Dominik Moritz <domoritz@gmail.com>
2018-06-26 00:49:22 -0700

    Tooltip in line chart

--- src/vega-linechart.ts
+++ src/vega-linechart.ts
@@ -9,6 +9,12 @@ export default function(data) {
     )
     .reduce((acc, val) => acc.concat(val), []);
 
+  const encoding: any = {
+    color: { field: "group", type: "nominal", title: "Series" },
+    x: { field: "time", type: "quantitative", title: "Time" },
+    y: { field: "value", type: "quantitative", title: "Value" }
+  };
+
   embed(
     document.getElementById("lines"),
     {
@@ -22,9 +28,8 @@ export default function(data) {
         orient: "vertical"
       },
       encoding: {
-        color: { field: "group", type: "nominal", title: "Series" },
-        x: { field: "time", type: "quantitative", title: "Time" },
-        y: { field: "value", type: "quantitative", title: "Value" }
+        ...encoding,
+        tooltip: Object.keys(encoding).map(k => encoding[k])
       }
     },
     { defaultStyle: true }
